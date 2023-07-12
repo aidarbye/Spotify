9aff3053e8134e320c26960801dc9319dfc9c036
Aidar <aidar.nurkin@icloud.com>
2023-07-12 20:37:20 +0600

    launchscreen

--- spotify/Managers/AuthManager.swift
+++ spotify/Managers/AuthManager.swift
@@ -110,12 +110,12 @@ final class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool)-> Void ) {
+    public func refreshIfNeeded(completion: ((Bool)-> Void)?) {
         guard !refreshingToken else {
             return
         }
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         guard let refreshToken = self.refreshToken else {
@@ -142,7 +142,7 @@ final class AuthManager {
         let basicToken = Constants.clientID+":"+Constants.clientSecret
         let data = basicToken.data(using: .utf8)
         guard let base64string = data?.base64EncodedString() else {
-            completion(false)
+            completion?(false)
             print("Failure to get base64")
             return
         }
@@ -152,7 +152,7 @@ final class AuthManager {
         let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             self?.refreshingToken = false
             guard let data = data, error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             do {
@@ -160,10 +160,10 @@ final class AuthManager {
                 self?.onRefreshBlocks.forEach { $0(result.access_token) }
                 self?.onRefreshBlocks.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             } catch {
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
         }
         task.resume()
